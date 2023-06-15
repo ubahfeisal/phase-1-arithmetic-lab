47cff95622db05ef2555634484c3e58f811039db
ubah feisal <feisalubah1@gmail.com>
2023-06-15 20:11:03 +0300

    completed the assignment

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 
 // Write your code here
+const num1 = 62;
+const num2 = 1;
+const multiply = num1 * num2;
+console.log(multiply);
+
+const num3 = 16;
+const num4 = 6;
+const mod = num3 % num4;
+console.log(mod);
+
+
+const max = Math.max (18 ,13, 5, 16, 20);
+console.log (max);
+
+const random = Math.floor (Math.random()) +1;
